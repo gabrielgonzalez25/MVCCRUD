e680e9fe52c5d15208d1891dab4ff376f5a6ad86
gabriel jose gonzalez <gabrieljogoho@hotmail.com>
2020-11-30 20:02:30 -0500

    Final crud

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,10 +1,11 @@
-﻿ using System;
+﻿  using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using MVCCrud1.Models;
 using MVCCrud1.Models.TableViewModels;
+using MVCCrud1.Models.ViewModels;
 
 namespace MVCCrud1.Controllers
 {
@@ -29,11 +30,97 @@ namespace MVCCrud1.Controllers
             }
 
 
-                return View(lst);
+            return View(lst);
+        }
+        [HttpGet]
+
+        public ActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Add(ViewModel model)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            using (var db = new userEntities())
+            {
+                usermvc oUser = new usermvc();
+                oUser.idState = 1;
+                oUser.email = model.Email;
+                oUser.password = model.Password;
+                oUser.edad = model.Edad;
+
+                db.usermvc.Add(oUser);
+                db.SaveChanges(); 
+
+
+            }
+            return Redirect(Url.Content("~/User/"));
+
+
+
         }
 
+        public ActionResult Edit(int Id)
+        {
+            EditViewModel model = new EditViewModel();
 
+            using(var db = new userEntities())
+                {
+                var oUser = db.usermvc.Find(Id);
+                model.Edad = (int)oUser.edad;
+                model.Email = oUser.email;
+                model.Id = oUser.id;
+            }
 
+            return View(model);
+        }
+        [HttpPost]
+        public ActionResult Edit(EditViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
 
-    }
-}
+            using (var db = new userEntities())
+            {
+                var oUser = db.usermvc.Find(model.Id);
+                oUser.email = model.Email;
+                oUser.edad = model.Edad;
+
+                if(model.Password!=null && model.Password.Trim() != "")
+                {
+                    oUser.password = model.Password;
+                }
+
+                db.Entry(oUser).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+
+            }
+
+            return Redirect(Url.Content("~/User/"));
+        }
+        [HttpPost]
+        public ActionResult Delete(int Id)
+        {
+
+            using (var db = new userEntities())
+            {
+                var oUser = db.usermvc.Find(Id);
+                oUser.idState = 3; //pa eliminar
+
+                db.Entry(oUser).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+
+            }
+
+            return Content("1");
+        }
+    }    
+  }  
--- Models/ViewModels/ViewModel.cs
+++ Models/ViewModels/ViewModel.cs
@@ -13,14 +13,39 @@ namespace MVCCrud1.Models.ViewModels
         [EmailAddress]
         [Display(Name ="Correo electronico")]
         [DataType(DataType.EmailAddress)]
-        [StringLength(100, ErrorMessage ="", MinimumLength = 5)]
+        [StringLength(100, ErrorMessage ="Debe contener minimo 5 caracteres", MinimumLength = 5)]
         public string Email { get; set; }
         [Required]
         [DataType(DataType.Password)]
         [Display(Name = "Contraseña")]
         public string Password { get; set; }
+        [Required]
         [DataType(DataType.Password)]
         [Display(Name = "Confirmar contraseña")]
+        [Compare("Password",ErrorMessage ="Las contraseñas no son iguales")]
+        public string ConfirmPassword { get; set; }
+        [Required]
+        public int Edad { get; set; }
+    }
+
+    public class EditViewModel
+    {
+        public long Id { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Correo electronico")]
+        [DataType(DataType.EmailAddress)]
+        [StringLength(100, ErrorMessage = "Debe contener minimo 5 caracteres", MinimumLength = 5)]
+        public string Email { get; set; }
+        
+        [DataType(DataType.Password)]
+        [Display(Name = "Contraseña")]
+        public string Password { get; set; }
+        
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirmar contraseña")]
+        [Compare("Password", ErrorMessage = "Las contraseñas no son iguales")]
         public string ConfirmPassword { get; set; }
         [Required]
         public int Edad { get; set; }
